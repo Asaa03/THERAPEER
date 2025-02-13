86bf4bf1e33cf0714bf3b5dba0576b0572dcff57
Asaa03 <151516100+Asaa03@users.noreply.github.com>
2025-02-13 14:17:52 +0545

    Update AdminContext.jsx

--- frontend/src/context/AdminContext.jsx
+++ frontend/src/context/AdminContext.jsx
@@ -8,7 +8,7 @@ const AdminContextProvider = (props) => {
     const [aToken,setAToken] = useState(localStorage.getItem('aToken') ? localStorage.getItem('aToken') : '')
     const [therapists,setTherapists] = useState([])
     const [appointments, setAppointments] = useState([])
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = 'https://therapeer-backend.onrender.com'
     const [dashData, setDashData] = useState(false)
 
 
