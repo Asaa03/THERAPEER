0558b5c44a371f702b73f62ec90a6e62977a8998
Asaa03 <151516100+Asaa03@users.noreply.github.com>
2025-02-13 14:18:28 +0545

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext()
 const AppContextProvider = (props) => {
 
     const currencySymbol = 'Rs.'
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = 'https://therapeer-backend.onrender.com'
 
     const [therapists, setTherapists] = useState([])
     const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false)
